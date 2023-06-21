a37b368c17246d4313a33cde44289ce002cff600
Vladislav Pyzhov <vladpyzhov@gmail.com>
2023-06-21 15:24:36 +1000

    Plotting utils change

---
+++ ds_utils/plotting/plot_utils.py
@@ -0,0 +1,188 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import plotly.graph_objects as go
+
+from ..preprocessing_utils import EllipseShape
+
+
+def get_plotly_shape(x0: float, x1: float, y0: float, y1: float, dash: str = 'solid', opacity: float = 1, color: str='blue') -> dict:
+    """
+    Function to construct a plotly shape objects to be used in the Plotly figure
+
+    Parameters
+    ----------
+    x0 : float
+        Lowest x value
+    x1 : float
+        Highest x value
+    y0 : float
+        Lowest y value
+    y1 : float
+        Highest y value
+    dash : str, optional
+        Type of the edge line to use, by default 'solid'
+    opacity : float, optional
+        Coefficient to control the opacity of the figure edges, by default 1
+    color: str, optiona;
+        Color of the resulted shape, by default blue
+
+    Returns
+    -------
+    dict
+        Plotly shape dictionary
+    """
+    return {'type': 'line', 'x0': x0, 'x1': x1, 'y0': y0, 'y1': y1, 
+            'line': dict(color=color, width=1, dash=dash), 'opacity': opacity}
+
+
+def plot_scatter_with_ellipse(input_df: pd.DataFrame, one_col_name: str, two_col_name: str, 
+                              proportion: float = 0.9, plot_ols: bool = True, suffix: str = '') -> go.Figure:
+    """
+    Function to create a scatter plot with an ellipse fitted to separate the main proportion of data from outliers.
+
+    Parameters
+    ----------
+    input_df : pd.DataFrame
+        Input dataframe
+    one_col_name : str
+        Feature one
+    two_col_name : str
+        Feature two
+    proportion : float, optional
+        Proportion of main data to separate from outliers using an ellipse, by default 0.9
+    plot_ols : bool, optional
+        Whether to plot fitted OLS model or not, by default True
+    suffix : str, optional
+        Suffix to add to the titles of the resulted graph, by default ''
+
+    Returns
+    -------
+    go.Figure
+        Plotly scatter plot with a fitted ellipse
+    """
+    # Estimate an ellipse
+    title=f'Ellipse for {proportion} proportion {suffix}',
+    ellipse = EllipseShape.fit(input_df, one_col_name=one_col_name, two_col_name=two_col_name, proportion=proportion)
+    boundary_x_array, boundary_y_array = ellipse.get_boundary_points()
+    
+    # Prepare figure layout
+    max_value = max(input_df[one_col_name].max(), input_df[two_col_name].max())
+    min_value = max(input_df[one_col_name].min(), input_df[two_col_name].min())
+    layout = go.Layout(
+        title=title,
+        xaxis=dict(title=one_col_name, range=[min_value, max_value * 1.05]),
+        yaxis=dict(title=two_col_name, range=[min_value, max_value * 1.05]),
+        showlegend=False
+    )
+
+    # Get scatter plot with a fitted ellipse on it
+    fig = go.Figure(
+        data=[
+            # get ellipse object
+            go.Scatter(
+                x=boundary_x_array,
+                y=boundary_y_array,
+                mode='lines',
+                line=dict(color='blue', width=1),
+                name='Ellipse',
+                showlegend = False
+            ),
+            # plot all samples
+            go.Scatter(x=input_df[one_col_name].tolist(), 
+                       y=input_df[two_col_name].tolist(), 
+                       mode='markers', 
+                       # Make all inner points as blue markers and all outliers as red markers
+                       marker= {
+                           'color': ['blue' if ellipse.contains(v) else 'red' for v in input_df[[one_col_name, two_col_name]].values],
+                           },
+                       showlegend = False)
+            ], layout=layout)
+    
+    if plot_ols:
+        # Select only those samples that do fall into the ellipse
+        inner_points = [v for v in input_df[[one_col_name, two_col_name]].values if ellipse.contains(v)]
+        
+        # Calculate OLS estimates
+        ols_slope, ols_intercept, _, ols_p_value, _ = stats.linregress(*zip(*np.vstack(inner_points)))
+        
+        # Add a line to the plot
+        fig.add_shape(**get_plotly_shape(
+            min_value, max_value, 
+            ols_intercept + (min_value*ols_slope), ols_intercept + (max_value*ols_slope), 
+            color='blue', dash='dash'))
+        fig.update_layout(title=f'Ellipse for {proportion} with OLS slope {round(ols_slope, 2)} (p-value {round(ols_p_value,2)}) {suffix}')
+    return fig
+
+
+def plot_spread_graph_with_shaded_areas(input_df: pd.DataFrame, column_one: str, column_two: str, title: str = '') -> go.Figure:
+    """
+    Function to plot a spread graph between two features, while also shading the area between them:
+    1. When feature 1 is greater than feature 2 - shaded area is green
+    2. When feature 1 is lower than feature 2 - shaded area is reds
+    
+    Parameters
+    ----------
+    input_df : pd.DataFrame
+        Input data frame with the features
+    column_one : str
+        Feature one
+    column_two : str
+        Feature two
+    title : str, optional
+        title of the resulted graph, by default ''
+
+    Returns
+    -------
+    go.Figure
+        Plotly figure
+    """
+    # Get a scatter plot for feature one
+    trace_one = go.Scatter(
+        x=input_df.index,
+        y=input_df[column_one],
+        name=column_one,
+        mode='lines',
+        line=dict(color='rgb(102,166,30)')
+    )
+    # Get a scatter plot for feature two
+    trace_two = go.Scatter(
+        x=input_df.index,
+        y=input_df[column_two],
+        name=column_two,
+        mode='lines',
+        line=dict(color='rgb(204,80,62)')
+    )
+    # Get a scatter for the green shaded area
+    trace_fill_one = go.Scatter(
+        x=input_df.index,
+        y=input_df[[column_one, column_two]].max(axis=1),
+        fill='tonext',
+        mode='lines',
+        fillcolor='rgba(10,255,10,0.3)',
+        line=dict(width=0),
+        showlegend=False,
+    )
+    # Get a scatter for the red shaded area
+    trace_fill_two = go.Scatter(
+        x=input_df.index,
+        y=input_df[[column_one, column_two]].min(axis=1),
+        fill='tonextx',
+        mode='lines',
+        fillcolor='rgba(255,0,0,0.1)',
+        line=dict(width=0),
+        showlegend=False,
+    )
+    # Update layout  
+    layout = go.Layout(
+        title=title
+    )
+    fig = go.Figure(data=[
+        trace_one, 
+        trace_two, 
+        trace_fill_one,
+        trace_fill_two
+        ], layout=layout
+    )
+    return fig
+    
---
+++ ds_utils/preprocessing_utils.py
@@ -0,0 +1,97 @@
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from scipy import stats
+from dataclasses import dataclass
+
+
+@dataclass
+class EllipseShape:
+    x_center: float
+    y_center: float
+    width: float
+    height: float
+    angle: float
+
+    @property
+    def center(self) -> Tuple[float, float]:
+        return self.x_center, self.y_center
+    
+    def contains(self, point: Tuple[float, float]) -> bool:
+        """
+        Checks if the ellipse contains the provided point
+        Parameters
+        ----------
+        point : tuple of float
+            Point coordinates
+
+        Returns
+        -------
+        Boolean
+            True if the point is inside the ellipse
+        """
+        # Convert the angle to radians
+        angle_rad = np.deg2rad(self.angle)
+
+        # Calculate the rotated coordinates
+        x_rot = (point[0] - self.x_center) * np.cos(angle_rad) + (point[1] - self.y_center) * np.sin(angle_rad)
+        y_rot = -(point[0] - self.x_center) * np.sin(angle_rad) + (point[1] - self.y_center) * np.cos(angle_rad)
+
+        # Calculate the values to check against the ellipse equation
+        x_val = (x_rot / (self.width / 2)) ** 2
+        y_val = (y_rot / (self.height / 2)) ** 2
+
+        if x_val + y_val <= 1:
+            return True
+        return False
+
+    @classmethod
+    def fit(cls, input_df: pd.DataFrame, one_col_name: str, two_col_name: str, proportion: float = 0.95) -> 'EllipseShape':
+        """
+        Function to fit an ellipse that would separate given proportion of data samples from outliers.
+        Visually - on the graph with sample points fits the ellipse that will include at least *proportion* of elements
+
+        Parameters
+        ----------
+        input_df : pd.DataFrame
+            Input dataframe
+        one_col_name : str
+            First column name
+        two_col_name : str
+            Second column name
+        proportion : float, optional
+            Percentage of samples to be inside the ellipse, by default 0.95
+
+        Returns
+        -------
+        EllipseShape
+            Fitted ellipse shape
+        """
+        means = input_df[[one_col_name, two_col_name]].mean()
+        covariance_matrix = np.cov(input_df[one_col_name], input_df[two_col_name])
+        # To make the figure axis-aligned - use eigen values and eigen vectors
+        eig_val, eig_vectors = np.linalg.eig(covariance_matrix)
+        # The orientation of the ellips could be derived using
+        angle = np.degrees(np.arctan2(*eig_vectors[:, 0][::-1]))
+        confidence_coef = stats.chi2.ppf(proportion, 2)
+        return cls(
+            x_center=means[0],
+            y_center=means[1],
+            width=2 * np.sqrt(confidence_coef * eig_val[0]),
+            height=2 * np.sqrt(confidence_coef * eig_val[1]),
+            angle=angle)
+    
+    def get_boundary_points(self) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Return coordinates for columns that lie on the ellipse boundaries
+        Returns
+        -------
+        Tuple of arrays
+            X and Y coordinates
+        """
+        theta = np.linspace(0, 2 * np.pi, 100)
+        angle_rad = np.deg2rad(self.angle)
+        x = self.x_center + (self.width / 2) * np.cos(theta) * np.cos(angle_rad) - (self.height / 2) * np.sin(theta) * np.sin(angle_rad)
+        y = self.y_center + (self.width / 2) * np.cos(theta) * np.sin(angle_rad) + (self.height / 2) * np.sin(theta) * np.cos(angle_rad)
+        return x, y
